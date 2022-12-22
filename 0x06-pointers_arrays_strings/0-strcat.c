60d9a9d42cc6bad12d1aaa780806997f0f958d10
Wambugubetty <bemkar14@gmail.com>
2022-12-22 04:43:16 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,10 @@
  */                                                                                                                                             
                                                                                                                                                 
 char *_strcat(char *dest, char *src)                                                                                                            
-	                                                                                                                                                
-{                                                                                                                                              	        int dlen = 0, i;                                                                                                                        
-		                                                                                                                               
-	while (dest[dlen])                                                                                                                     
+	                                                                                                                                        
+{                                                                                                                                              	        int dlen = 0, i;
 
+	while (dest[dlen])
 	{                                                                                                                                      
 		dlen++;                                                                                                                         
 	}
@@ -22,8 +21,8 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; src[i] != 0; i++)       
     	{                                                                                                                         	
 		dest[dlen] = src[i];                                                                                                            		dlen++; 
-	}                                       	
+	}                      	
 
-	dest[dlen] = '\0';                                                                                                                     
+	dest[dlen] = '\0';  
 	return (dest);                                                                                                                          
 }             

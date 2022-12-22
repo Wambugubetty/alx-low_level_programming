391a5c6de4c0e8e503bd7508fe65eabeb991bb8c
Wambugubetty <bemkar14@gmail.com>
2022-12-22 04:49:41 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,15 @@
 #include "main.h"                                                                                                                               
 #include <stdio.h>                                                                                                                              
-                                                                                                                                                
+   
+
 /**                                                                                                                                             
  *  _strcat - Concatenates two strings                                                                                                          
  *  @dest: The destination string                                                                                                               
  *  @src: The source string                                                                                                                     
  *  Return: A pointer to the resulting string dest                                                                                              
  */                                                                                                                                             
-                                                                                                                                                
+        
+
 char *_strcat(char *dest, char *src)                                                                                                            
 	                                                                                                                                        
 {                                                                                                                                              	        int dlen = 0, i;
@@ -16,6 +18,7 @@ char *_strcat(char *dest, char *src)
 	{                                                                                                                                      
 		dlen++;                                                                                                                         
 	}
+
 
 
 	for (i = 0; src[i] != 0; i++)       

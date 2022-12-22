b85e0fd7ef83dfa3fe7c9d8a9bc212af353e5127
Wambugubetty <bemkar14@gmail.com>
2022-12-22 13:56:10 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,13 @@
-#include "main.h"                                                                                                                                     
-                                                                                                                                                      
+#include "main.h"                                                                                                                                                           
 /**                                                                                                                                                   
  * cap_string - Capitalizes all words of a string.                                                                                                    
  * @str: The string to be capitalized.                                                                                                                
  * Return: A pointer to the changed string.                                                                                                           
  */                                                                                                                                                   
                                                                                                                                                       
-char *cap_string(char *str)                                                                                                                           
-                                                                                                                                                      
+char *cap_string(char *str)                                                                                                                                                                                                                                                   
 {                                                                                                                                                     
-        int index = 0;                                                                                                                                
-                                                                                                                                                      
+        int index = 0;                                                                                                                                                                                               
         while (str[index])                                                                                                                            
         {                                                                                                                                             
         while (!(str[index] >= 'a' && str[index] <= 'z'))                                                                                             
@@ -32,6 +29,5 @@ char *cap_string(char *str)
         str[index] -= 32;                                                                                                                             
         index++;                                                                                                                                      
         }                                                                                                                                             
-        return (str);                                                                                                                                 
-                                                                                                                                                      
+        return (str);                                                                                                          
 }  
